b1b3fc6ac9931c2fb1a16e52682567b3214e80a1
Duc Trung LE <leductrungxf@gmail.com>
2021-11-04 17:49:35 +0100

    Lint code

--- nb_offline_convert/__init__.py
+++ nb_offline_convert/__init__.py
@@ -1,2 +1,4 @@
 from .offline_HTML import OfflineHTMLExpoter
 from .offline_WebPDF import OfflineWebPDFExpoter
+
+__all__ = ["OfflineHTMLExpoter", "OfflineWebPDFExpoter"]
--- nb_offline_convert/offline_HTML.py
+++ nb_offline_convert/offline_HTML.py
@@ -1,12 +1,11 @@
 from nbconvert import HTMLExporter
-from traitlets.traitlets import Dict
 from .tools import OfflineMixing
 
 
 class OfflineHTMLExpoter(OfflineMixing, HTMLExporter):
 
     template_name = "lab_offline"
-    
+
     def from_notebook_node(self, nb, resources=None, **kw):
         resources["offline_module_path"] = self.offline_module_path
         return super().from_notebook_node(nb, resources, **kw)
